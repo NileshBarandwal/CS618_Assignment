0e47b831eb6858d5decc088d219746a4cbdb5b54
NileshBarandwal <nbarandwal@gmail.com>
2024-08-11 15:36:12 +0530

    CS618 || Assignment || Lab 1 || Q3 || updated script to take folder directory dynamically

--- Assignment_2/Q3/ten_dirs.sh
+++ Assignment_2/Q3/ten_dirs.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-mkdir "ten"
+# Get the current working directory and append "/ten" to it
+TARGET_DIR="$(pwd)/ten"
 
-# Define the target directory
-TARGET_DIR="/home/nilesh/Assignment2/Question_3/ten"
+# Create the target directory
+mkdir -p "$TARGET_DIR"
 
-#The -w option pads numbers with leading zeros so that all numbers have the same width (e.g., 01, 02, ..., 10).
+# The -w option pads numbers with leading zeros so that all numbers have the same width (e.g., 01, 02, ..., 10).
 for i in $(seq -w 1 10)
 do
     # Define the directory name with the target path
     DIR_NAME="$TARGET_DIR/dir$i"
-    
+
     # Create the directory
-    mkdir "$DIR_NAME"
-    
+    mkdir -p "$DIR_NAME"
+
     for file_num in $(seq 1 4)
     do
-	    FILE_NAME="$DIR_NAME/file$file_num.txt"
-	    touch "$FILE_NAME"
-	    for ((line=1; line<=file_num; line++))
-            do
-              	echo "$file_num" >> "$FILE_NAME"
-            done
-	    echo "File '$FILE_NAME' created."
+        FILE_NAME="$DIR_NAME/file$file_num.txt"
+        touch "$FILE_NAME"
+        for ((line=1; line<=file_num; line++))
+        do
+            echo "$file_num" >> "$FILE_NAME"
+        done
+        echo "File '$FILE_NAME' created."
     done
 
     # Optional: Print a message confirming the creation
