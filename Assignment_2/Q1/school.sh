dae63cf43ae4fbaa91c0ce83ac66dc8dfb9006ff
NileshBarandwal <nbarandwal@gmail.com>
2024-08-11 15:45:21 +0530

    CS618 || Assignment || Lab 1 || Q2 || Find Average of TotalAssessedValue

--- Assignment_2/Q1/school.sh
+++ Assignment_2/Q1/school.sh
@@ -3,5 +3,5 @@
 cat Property_Tax_Roll.csv | \
 grep "MADISON SCHOOL" | \
 cut -d',' -f7 | \
-awk '{sum += $1; count++} END {if (count > 0) printf "%.2f\n", sum / count; else print "No data for MADISON SCHOOLS"}'
+awk '{sum += $1; count++} END {if (count > 0) printf "Average : %.2f\n", sum / count; else print "No data for MADISON SCHOOLS"}'
 
